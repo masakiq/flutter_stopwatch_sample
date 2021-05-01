e4dcf76154d0cc4141d7bd01ef2ae7ba8b242c19
masakiq <>
2021-05-01 15:32:07 +0900

    use stop_watch_timer lib
    new file:   lib/count_down_timer_page.dart
    new file:   lib/count_up_timer_page.dart
    modified:   lib/main.dart
    modified:   pubspec.lock
    modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,75 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
+import 'package:stop_watch_sample/count_up_timer_page.dart';
+import 'package:stop_watch_sample/count_down_timer_page.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-String formatTime(int milliseconds) {
-  var secs = milliseconds ~/ 1000;
-  var hours = (secs ~/ 3600).toString().padLeft(2, '0');
-  var minutes = ((secs % 3600) ~/ 60).toString().padLeft(2, '0');
-  var seconds = (secs % 60).toString().padLeft(2, '0');
-
-  return "$hours:$minutes:$seconds";
-}
-
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Stopwatch Example', home: StopwatchPage());
+    return MaterialApp(
+      home: MainPage(),
+    );
   }
 }
 
-class StopwatchPage extends StatefulWidget {
-  @override
-  _StopwatchPageState createState() => _StopwatchPageState();
-}
-
-class _StopwatchPageState extends State<StopwatchPage> {
-  Stopwatch _stopwatch;
-  Timer _timer;
-
-  @override
-  void initState() {
-    super.initState();
-    _stopwatch = Stopwatch();
-    _timer = new Timer.periodic(new Duration(milliseconds: 30), (timer) {
-      setState(() {});
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
-  void handleStartStop() {
-    if (_stopwatch.isRunning) {
-      _stopwatch.stop();
-    } else {
-      _stopwatch.start();
-    }
-
-    setState(() {});
-  }
-
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Stopwatch Example')),
+      appBar: AppBar(
+        title: const Text('Plugin example app'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(formatTime(_stopwatch.elapsedMilliseconds),
-                style: TextStyle(fontSize: 48.0)),
-            ElevatedButton(
-                onPressed: handleStartStop,
-                child: Text(_stopwatch.isRunning ? 'Stop' : 'Start')),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.pinkAccent,
+                  padding: const EdgeInsets.all(4),
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {
+                  CountUpTimerPage.navigatorPush(context);
+                },
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    'Go To CountUpTimer',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 4),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.pinkAccent,
+                  padding: const EdgeInsets.all(4),
+                  shape: const StadiumBorder(),
+                ),
+                onPressed: () {
+                  CountDownTimerPage.navigatorPush(context);
+                },
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16),
+                  child: Text(
+                    'Go To CountDownTimer',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
